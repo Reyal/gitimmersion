707f3432cefba9019e01ea1a71ef10590f7cfc5d
Nick Christ <nchrist01@gmail.com>
2014-10-19 22:41:47 +0900

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,9 @@
+require 'greeter'
+
 # Default is "World"
 # Author: Nick Christ (nick@somewhere.com)
 name = ARGV.first || "World"
-puts "Hello #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
+
